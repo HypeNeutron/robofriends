715c6b636120016c6f57d3673091fd89b16af31a
Oam surachet <surachat2014@gmail.com>
2021-03-20 11:51:54 +0700

    add Pagekage depolying and catching

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./scroll.css";
 export const Scroll = ({ children }) => {
-  return <div className="scrolls">{children}</div>;
+  try {
+    return <div className="scrolls">{children}</div>;
+  } catch (error) {
+    throw new Error("NO");
+  }
 };
